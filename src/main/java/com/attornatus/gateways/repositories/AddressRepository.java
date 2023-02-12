9a3e8035e6fc488ac6c822ab997a2957f6f3ea5a
Paulo <deoliveira.pe@gmail.com>
2023-02-12 18:19:50 -0300

    feat: Repositories interfaces for contacts and addresses

--- src/main/java/com/attornatus/gateways/repositories/AddressRepository.java
+++ src/main/java/com/attornatus/gateways/repositories/AddressRepository.java
@@ -1,10 +1,12 @@
-package com.attornatus.gateways;
+package com.attornatus.gateways.repositories;
 
 import com.attornatus.entities.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, String> {
     List<Address> findByContactId(String contactId);
     List<Address> findByContactIdAndPrimaryAddress(String contactId);
--- src/main/java/com/attornatus/gateways/repositories/ContactRepository.java
+++ src/main/java/com/attornatus/gateways/repositories/ContactRepository.java
@@ -1,7 +1,9 @@
-package com.attornatus.gateways;
+package com.attornatus.gateways.repositories;
 
 import com.attornatus.entities.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContactRepository extends JpaRepository<Contact, String> {
 }
