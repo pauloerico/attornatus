de07b58bea997e6afa9db9b1f14ea8986e22b70f
Paulo <deoliveira.pe@gmail.com>
2023-02-12 23:55:44 -0300

    feat: Controllers

--- src/main/java/com/attornatus/gateways/controllers/AddressController.java
+++ src/main/java/com/attornatus/gateways/controllers/AddressController.java
@@ -1,2 +1,62 @@
-package com.attornatus.gateways.controllers;public class AddressController {
+package com.attornatus.gateways.controllers;
+
+import com.attornatus.entities.Address;
+import com.attornatus.entities.Contact;
+import com.attornatus.gateways.dtos.AddressDto;
+import com.attornatus.gateways.repositories.AddressRepository;
+import com.attornatus.gateways.repositories.ContactRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class AddressController {
+
+    private final ContactRepository contactRepository;
+    private final AddressRepository addressRepository;
+
+    public AddressController(ContactRepository contactRepository, AddressRepository addressRepository) {
+        this.contactRepository = contactRepository;
+        this.addressRepository = addressRepository;
+    }
+
+    @GetMapping("/contacts/{contactId}/addresses")
+    public ResponseEntity<List<Address>> getAllContactAddresses(@PathVariable String contactId){
+        if (!contactRepository.existsById(contactId)){
+            throw new EntityNotFoundException("Contact not found with id = " + contactId);
+        }
+        List<Address> addresses = addressRepository.findByContactId(contactId);
+        return new ResponseEntity<>(addresses, HttpStatus.OK);
+    }
+
+    @PostMapping("/contacts/{contactId}/addresses")
+    public ResponseEntity<Address> newAddress(@PathVariable String contactId, @RequestBody @Valid AddressDto addressDto){
+        Contact contact = contactRepository.findById(contactId)
+                .orElseThrow(()->new EntityNotFoundException("Contact not found with id = " + contactId));
+        Address address = new Address();
+        BeanUtils.copyProperties(addressDto, address);
+        address.setContact(contact);
+        addressRepository.save(address);
+        return new ResponseEntity<>(address, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/addresses/{id}")
+    public ResponseEntity<Address> editAddress(@PathVariable String id, @RequestBody @Valid AddressDto addressDto){
+        Address address = addressRepository.findById(id)
+                .orElseThrow(()->new EntityNotFoundException("Address not found with id = " + id));
+        BeanUtils.copyProperties(addressDto, address);
+        addressRepository.save(address);
+        return new ResponseEntity<>(address, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/addresses/{id}")
+    public ResponseEntity<HttpStatus> deleteAddress(@PathVariable String id){
+        addressRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
--- src/main/java/com/attornatus/gateways/controllers/ContactController.java
+++ src/main/java/com/attornatus/gateways/controllers/ContactController.java
@@ -1,2 +1,63 @@
-package com.attornatus.gateways.controllers;public class ContactController {
+package com.attornatus.gateways.controllers;
+
+import com.attornatus.entities.Contact;
+import com.attornatus.gateways.dtos.ContactDto;
+import com.attornatus.gateways.repositories.ContactRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/contacts")
+public class ContactController {
+
+    private final ContactRepository contactRepository;
+
+    public ContactController(ContactRepository contactRepository){
+        this.contactRepository = contactRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Contact>> getAllContacts(){
+        List<Contact> contacts = contactRepository.findAll();
+        if (contacts.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(contacts, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Contact> newContact(@RequestBody @Valid ContactDto contactDto){
+        Contact contact = new Contact();
+        BeanUtils.copyProperties(contactDto, contact);
+        contactRepository.save(contact);
+        return new ResponseEntity<>(contact, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Contact> getContactById(@PathVariable String id){
+        Contact contact = contactRepository.findById(id)
+                .orElseThrow(()->new EntityNotFoundException("Contact not found with id = " + id));
+        return new ResponseEntity<>(contact, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Contact> editContact(@PathVariable String id, @RequestBody @Valid ContactDto contactDto){
+        Contact contact = contactRepository.findById(id)
+                .orElseThrow(()->new EntityNotFoundException("Contact not found with id = " + id));
+        BeanUtils.copyProperties(contactDto, contact);
+        contactRepository.save(contact);
+        return new ResponseEntity<>(contact, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteContact(@PathVariable String id){
+        contactRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
