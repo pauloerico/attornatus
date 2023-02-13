341e29e732fd266d1af6887693aea3b1c2133ee2
Paulo <deoliveira.pe@gmail.com>
2023-02-12 23:55:13 -0300

    feat: DTOs

--- src/main/java/com/attornatus/gateways/dtos/AddressDto.java
+++ src/main/java/com/attornatus/gateways/dtos/AddressDto.java
@@ -1,2 +1,27 @@
-package com.attornatus.gateways.dtos;public class AddressDto {
+package com.attornatus.gateways.dtos;
+
+import com.attornatus.entities.Contact;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Data
+public class AddressDto {
+    @NotBlank
+    private String street;
+    @NotBlank
+    private int number;
+    @NotBlank
+    private String city;
+    @NotBlank
+    private String state;
+    @NotBlank
+    private int cep;
+    @NotBlank
+    private boolean isPrimaryAddress;
 }
--- src/main/java/com/attornatus/gateways/dtos/ContactDto.java
+++ src/main/java/com/attornatus/gateways/dtos/ContactDto.java
@@ -1,2 +1,14 @@
-package com.attornatus.gateways.dtos;public class ContactDto {
+package com.attornatus.gateways.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class ContactDto {
+    @NotBlank
+    private String name;
+    @NotBlank
+    private LocalDate dateOfBirth;
 }
